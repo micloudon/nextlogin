292b05cbeb2dbc0cbf2e616bd022dd53cf70aadf
mic <micloudon@gmail.com>
2021-01-25 16:32:28 -0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    env: {
+        API_KEY_ID: process.env.API_KEY_ID
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,43 @@
+import { useEffect } from "react";
+import Cotter from "cotter"; 
+import { useRouter } from "next/router";
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Grid from '@material-ui/core/Grid';
 
+console.log(process.env.API_KEY_ID)
 export default function Home() {
+  const router = useRouter();
+  //  2️⃣ Initialize and show the form
+  useEffect(() => {
+    var cotter = new Cotter(process.env.API_KEY_ID); 
+    cotter
+      .signInWithLink() 
+      .showEmailForm() 
+      .then((response) => {
+        console.log(response); 
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    
+    <div className="mainPage">
+        <Head>
+          <title>Buddy Tree</title>
+          <link rel="shortcut icon" href="/dog.png" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Grid className="box" container justify = "center">
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <div className="form" id="cotter-form-container" style={{ width: 400, height: 425 }}>
+        <h1 className="h" align="center">Log in</h1>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        </div> 
+        
+        </Grid>
+  
+        
+      </div>
+  );
 }
